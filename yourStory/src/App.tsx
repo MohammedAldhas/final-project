11429732e39757a55e16dc62948446c17908dc8f
gharamalhumaydani <gh.alhumaydani@gmail.com>
2023-09-13 17:18:19 +0300

    log/sign

--- yourStory/src/App.tsx
+++ yourStory/src/App.tsx
@@ -1,13 +1,16 @@
 
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+
+import LogInPage from "./pages/LogInPage";
 
 
-import "./App.css";
+// import "./App.css";
+// // import ChatGPT from "./components/ChatGPT";
+// import { Routes,Route } from "react-router-dom";
+// import BooksUser from "./components/BooksUser";
 // import ChatGPT from "./components/ChatGPT";
-import { Routes,Route } from "react-router-dom";
-import BooksUser from "./components/BooksUser";
-import ChatGPT from "./components/ChatGPT";
-import OurStory from "./pages/OurStory";
+// import OurStory from "./pages/OurStory";
+
 
 
 
@@ -16,19 +19,10 @@ function App() {
 
   
   return (
+   
+     <LogInPage/>
 
-
-    <>
-
-<Routes>
-
-  <Route path="/" element={<OurStory></OurStory>}></Route>
-  <Route path="/user" element={<ChatGPT></ChatGPT>}></Route>
-  <Route path="/:id" element={<BooksUser></BooksUser>}></Route>
-</Routes>
-        
-    </>
-
+   
 
   );
 }
